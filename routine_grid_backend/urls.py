600f3772f2f0a890693102f36122a37ddf896300
Heet Patel <heetkpatel30@gmail.com>
2025-05-04 13:39:18 +0100

    Feat: Configure DRF, JWT, and add auth/registration endpoints

--- routine_grid_backend/urls.py
+++ routine_grid_backend/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from apps.users.views import RegisterView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.habits.urls")),
+    path("api/v1/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/v1/auth/register/", RegisterView.as_view(), name="auth_register"),
 ]
