9b1a5f4d637a5125c53fd01affbf11d114f21d98
Heet Patel <heetkpatel30@gmail.com>
2025-05-04 14:54:08 +0100

    Feat: Update Settings to use PostgreSQL with a SQLite fallback

--- routine_grid_backend/settings.py
+++ routine_grid_backend/settings.py
@@ -10,21 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
+import dj_database_url
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5r9#!4c2gjvioq_w#1auw!*yl6a)r-!%q0%qfusx--g_yxa2@="
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-fallback-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "true"
 
 ALLOWED_HOSTS = []
 
@@ -78,10 +84,10 @@ WSGI_APPLICATION = "routine_grid_backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"'sqlite:///{BASE_DIR}/db.sqlite3'",
+        conn_max_age=600,
+    )
 }
 
 
